0c424feb5941fa287a852b0fc79fd22efa0ca237
Екатерина Новоторцева <Gomozova.ekaterin@gmail.com>
2021-12-11 03:20:18 +0300

    LP

---
+++ 1.js
@@ -0,0 +1,5 @@
+function addProduct() {
+    //информация о продукте
+    var productAdded = $('<li class="product"><div class="product-image"><a href="#0"><img src="img/product-preview.png" alt="placeholder"></a></div><div class="product-details"><h3><a href="#0">Название продукта</a></h3><span class="price">Цена</span><div class="actions"><a href="#0" class="delete-item">Удалить</a><div class="quantity"><label for="cd-product-'+ productId +'">Количество</label><span class="select"><select id="cd-product-'+ productId +'" name="quantity"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select></span></div></div></div></li>');
+    cartList.prepend(productAdded);
+    }
